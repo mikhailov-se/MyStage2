f077dd96ae0c7d3d5ab83dc9fbdc8a1412aad917
mikhailov.se <mikhailov.se@hotmail.com>
2020-02-02 09:30:26 +0200

    Add  model

---
+++ MyStage2/Models/Announsment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyStage2.Models
+{
+    public class Announsment
+    {
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        [DataType(DataType.Date)] public DateTime CreateDate { get; set; }
+
+        [MaxLength(300)] public string TextAnnounsment { get; set; }
+
+
+        [Range(typeof(int), "1", "10")] public int Rating { get; set; }
+
+        public User User { get; set; }
+    }
+}
